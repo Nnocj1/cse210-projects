da47689977050c7ccf92e5604ba20d3884fccf2a
Nnocj1 <nicholascommey2001@gmail.com>
2024-01-14 18:13:44 +0000

    Initial Commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        //Prompt user for first and last name
+        Console.WriteLine("What is your first name?: ");
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name?: ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"{lname}, {fname} {lname}");
+
+
+        
     }
 }
