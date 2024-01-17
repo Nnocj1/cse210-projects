8a4d77c15422105867b6c51713d93a1e58b75398
Nnocj1 <nicholascommey2001@gmail.com>
2024-01-17 08:47:11 +0000

    C# Programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,3 +1,6 @@
+//A program that determines the letter grade for a course.
+/*Name: Nicholas Oblitey Commey
+  Date: 15/01/2024*/
 using System;
 
 class Program
@@ -5,5 +8,76 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        
+        
+
+        Console.Write("Enter your percentage score on any subject to see your grade: ");
+        string percentScore = Console.ReadLine();
+        int score = int.Parse(percentScore);
+        string sign;
+        string grade;
+          
+        if (score % 10 >= 7)
+        {
+            if (score >= 97)
+            {
+                sign = "";
+            }
+            else 
+            {
+                sign = "+";
+            }
+        }
+
+        else if (score % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        else 
+        {
+            sign = "";
+        }
+
+
+        if (score >= 90)
+        {
+           grade = "A";
+            Console.WriteLine($"Your grade is {grade}{sign}");
+        }
+
+        else if (score >=80)
+        {
+            grade = "B";
+            Console.WriteLine($"Your grade is {grade}{sign}");
+        }
+
+        else if (score >= 70)
+        {
+            grade = "c";
+            Console.WriteLine($"Your grade is {grade}{sign}");
+        }
+
+        else if (score >= 60)
+        {
+            grade = "D";
+            Console.WriteLine($"Your grade is {grade}{sign}");
+        }
+
+        else
+        {
+           grade = "F";
+           Console.WriteLine($"Your grade is {grade}{sign}");
+        }
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass the test. There is more room for improvement");
+        }
     }
+
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,3 +1,8 @@
+/* Name: An Assignment to practice on Loops in C#
+   Date: 17/01/2024
+   Author: Nicholas Oblitey Commey*/
+
+
 using System;
 
 class Program
@@ -5,5 +10,58 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        string playAgain = "yes";
+
+
+        //Ensure that each instance to try again, it starts counting afresh
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            
+            int guessNumber = -1;
+            int count = 0;
+            
+
+            Console.WriteLine("There is a hidden number. Guess what that number is");
+
+            //Keep asking for a guess till the answer matches the magic one.
+            while (magicNumber != guessNumber)
+            {   
+                Console.Write("What is your guess?: ");
+                string Number= Console.ReadLine();
+                guessNumber = int.Parse(Number);
+                
+
+                if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine("You are right!");
+                }
+
+                else if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Move higher");
+                }
+
+                else
+                {
+                    Console.WriteLine("Move lower");
+                }
+                
+                count++;
+                
+            }
+
+            Console.WriteLine($"You tried {count} times.");
+
+            Console.WriteLine("Would you like to play again? yes/no:");
+            string pAgain = Console.ReadLine();
+            playAgain = pAgain.ToLower(); 
+
+            
+        
+        }
     }
+        
 }
