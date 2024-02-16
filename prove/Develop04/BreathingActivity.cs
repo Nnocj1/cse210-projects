14b65131226c19b6afcdcd29f86c6330d3a12b50
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-16 16:43:47 +0000

    Completed Breathing Activity file

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,15 +1,33 @@
 using System;
 
+
 public class BreathingActivity: Activity
 {
     public BreathingActivity(string name, string description) : base(name, description)
-    {
-        
-
+    { //Place the variables in the Activity class.
+    
     }
 
     public void Run()
     {
-
-    }
+        DisplayStartingMessage();// Place  it here so that duration will obtain it value directly from this
+        
+        // Record the starting time
+        DateTime startTime = DateTime.Now;
+     
+        {
+            while(DateTime.Now - startTime <  TimeSpan.FromSeconds(GetDuration()))
+            {
+                Console.Write("Breathe in...");
+                ShowCountDown();
+                
+                Thread.Sleep(1000); // Wait for 1 second
+                Console.Write("Breathe out...");
+                ShowCountDown();
+                Thread.Sleep(1000); // Wait for 1 second
+            }
+           
+        }  
+        DisplayEndingMessage();
+    } 
 }
