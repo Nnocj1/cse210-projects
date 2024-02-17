9cbc1ce31d87b559b3dae4555cf6e2b3e6fd6326
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-17 09:11:17 +0000

    Program class completed

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,9 +2,10 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-
+       
         while (true)
         {
             Console.WriteLine("Menu Options:");
@@ -15,41 +16,40 @@ class Program
             Console.WriteLine("Select a choice from the menu");
             string userChoice = Console.ReadLine();
             int choice = int.Parse(userChoice);
-            
-            
-          
+
 
             if (choice == 1)
             {
                string name = "Breathing Activity";
-               string description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
-               
-               Activity activity = new Activity(name, activityInfo);
-               newActivity.DisplayStartingMessage();
+               string description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+
+               BreathingActivity newBreathingActivity = new BreathingActivity(name, description);
+               newBreathingActivity.Run();
+
             }
 
             else if (choice == 2)
             {
                string name = "Reflecting Activity";
-               string activityInfo = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
-               
-               Activity activity = new Activity(name, activityInfo);
-               newActivity.DisplayStartingMessage();
+               string description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+
+               //This section is the runing of the Reflecting Activity
+               ReflectingActivity newReflectingActivity = new ReflectingActivity(name, description);
+               newReflectingActivity.Run();
+          
             }
 
             else if (choice == 3)
             {
-                string name = "Listing Activity";
-                string activityInfo = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
-
-                Activity newActivity = new Activity(name, activityInfo);
-                newActivity.DisplayStartingMessage();
+                //string name = "Listing Activity";
+                break;
+                //string description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
             }
 
             else if (choice == 4)
             {
                 break;
-            }
+            }   
         }
     }
 }
