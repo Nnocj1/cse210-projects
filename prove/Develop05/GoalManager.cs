00d90309df2fcd0e9803732032633196e3c3c6d4
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-22 02:31:38 +0000

    further encapsulation

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -20,7 +20,51 @@ public class GoalManager
 
     public void Start()
     {
+        while (true)
+        {
+            //Displaying Total points earned.
+            DisplayPlayerInfo();
+            
+            //Menu Options
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Create New Goal\n 2. List Goals.\n 3. Save Goals.\n 4. Load Goals.\n 5. Record Event\n 6. Quit.");
+            Console.Write("Select a choice from the menu: ");
 
+            string userInput = Console.ReadLine();
+            int choice = int.Parse(userInput);
+            
+
+
+            if (choice == 1)
+            {
+                CreateGoal();
+            }
+
+            else if (choice == 2)
+            {
+                ListGoalDetails();
+            }
+
+            else if (choice == 3)
+            {
+                SaveGoals();
+            }
+
+            else if (choice == 4)
+            {
+                LoadGoals();
+            }
+
+            else if (choice == 5)
+            {
+                RecordEvent();
+            }
+
+            else if (choice == 6)
+            {
+                break;
+            }
+        }
     }
 
     public void DisplayPlayerInfo()
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,52 +8,9 @@ public class Program
         int score = 0;
         List<Goal> goals = new List<Goal>();
         GoalManager newGoalManager = new GoalManager(goals, score);
-       
-        while (true)
-        {
-            //Displaying Total points earned.
-            newGoalManager.DisplayPlayerInfo();
-            
-            //Menu Options
-            Console.WriteLine("Menu Options:");
-            Console.WriteLine(" 1. Create New Goal\n 2. List Goals.\n 3. Save Goals.\n 4. Load Goals.\n 5. Record Event\n 6. Quit.");
-            Console.Write("Select a choice from the menu: ");
 
-            string userInput = Console.ReadLine();
-            int choice = int.Parse(userInput);
-            
-
-
-            if (choice == 1)
-            {
-                newGoalManager.CreateGoal();
-            }
-
-            else if (choice == 2)
-            {
-                newGoalManager.ListGoalDetails();
-            }
-
-            else if (choice == 3)
-            {
-                newGoalManager.SaveGoals();
-            }
-
-            else if (choice == 4)
-            {
-                newGoalManager.LoadGoals();
-            }
-
-            else if (choice == 5)
-            {
-                newGoalManager.RecordEvent();
-            }
-
-            else if (choice == 6)
-            {
-                break;
-            }
-        }
+        newGoalManager.Start();
+    
     }
 }
 
