e22f26753a2dc71e3ea9ec1f8ac319b4a902f716
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-22 03:56:36 +0000

    final

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,7 +2,7 @@
 Purpose: Fulfil W05 Polymorphism Assessment.
 Lessons: I have learned the use of the key words virtual, abstract, and override used in applying polymorphism
 Date: 22/02/2024.
-Showing Creativity and exceeding requirement: I added a Display quote function to motivate users depending on their experience or expertise. */
+Showing Creativity and exceeding requirement: I added a Display quote function to motivate users depending on the type of goal set*/
 
 
 
