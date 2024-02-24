a760c260752b70c7d85a72399c3a40482e9380ff
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-24 17:15:41 +0000

    Add program comment for clarity

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,5 +1,14 @@
 using System;
+/* Author: Nicholas Oblitey Commey
+Purpose: CSE210 Final Project
+Program Purpose: This Program is to replicate the school administration system. It may be applied to 
+other organizations. 
+Program Inspiration: In Ghana, most schools run a paper based system which can be tedious, waste time as
+well as not reaching full potential. What if teachers can keep a digital lesson note which is easier to 
+maintain, or a system where calculations for student reports are automatically generated?.. We begin to see the benefits.
 
+Program Function: This program is to demonstrate some of the conditional logics that could exist in the 
+Software Application of a school.*/
 
 class Program
 {
